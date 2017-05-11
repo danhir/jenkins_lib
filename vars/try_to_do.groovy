36e346b8e21a86831cffb2c8b741055e5e9a8bc7
Jenkins_admin <jenkinsadm1@gmail.com>
2017-05-11 08:17:31 +0000

    using double quotes in shell cmd

--- vars/try_to_do.groovy
+++ vars/try_to_do.groovy
@@ -2,7 +2,7 @@ def call(cmd) {
     stage ('Trying to do stuff') {
        try {
          echo "Running command: ${cmd}"
-         sh '${cmd}'
+         sh "${cmd}"
        } catch (e) {
          echo "Failed to do stuff. Failure log: ${e}"
        }
