a839aca38d6bbde347e1435d02d2d746fda46556
Jenkins_admin <jenkinsadm1@gmail.com>
2017-05-11 08:01:05 +0000

    syntax fix for the stage

--- vars/try_to_do.groovy
+++ vars/try_to_do.groovy
@@ -1,8 +1,9 @@
 def call(cmd) {
-    stage 'Trying to do stuff'
-    try {
-        sh '${cmd}'
-    } catch (e) {
-        echo "Failed to do stuff. Failure log: ${e}"
+    stage ('Trying to do stuff') {
+       try {
+         sh '${cmd}'
+       } catch (e) {
+         echo "Failed to do stuff. Failure log: ${e}"
+       }
     }
 }
